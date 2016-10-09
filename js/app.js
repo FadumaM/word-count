d56b0d7f5e47c860b35f6f1413ec3c35f79f3f93
FadumaM <faduma.mohammed@gmail.com>
2016-10-09 22:36:32 +0100

    created word count javascript

--- js/app.js
+++ js/app.js
@@ -1,17 +1,42 @@
 window.onload = function() {
   var userInput = document.getElementById("userInput");
+  var wordLimitElement = document.getElementById("wordLimit");
+  var wordCountElement = document.getElementById("wordCount");
+  var wordLimitNumber = parseInt(wordLimitElement.innerHTML);
   bindEventListeners();
 
   function bindEventListeners() {
     userInput.addEventListener('keydown', getValue);
     userInput.addEventListener('change', getValue);
+    userInput.addEventListener('blur', getValue);
+    userInput.addEventListener('focus', getValue);
   }
 
   function getValue() {
     var regex = /\s+/gi;
     var value = userInput.value;
-    var wordCount = value.trim().replace(regex, ' ').split(' ').length;
-    console.log(wordCount);
+    if (value.length >= 1) {
+      var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+      // console.log(wordCount);
+      changeWordCount(wordCount);
+      changeTextColor(wordCount);
+    }
   }
 
+  function changeWordCount(wordCount) {
+    if (wordCount >= 1) {
+      console.log(wordLimitNumber);
+      wordLimitElement.innerHTML = wordLimitNumber - wordCount;
+      wordCountElement.innerHTML = wordCount;
+    }
+
+  }
+
+
+  function changeTextColor(wordCount) {
+    if (wordCount > wordLimitNumber) {
+      console.log("hi");
+      userInput.className = "overLimit";
+    }
+  }
 };
